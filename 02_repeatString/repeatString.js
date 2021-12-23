ec8b0c6a2ad08323662c953711a718ca7776ae52
Rodrigo <gokusan@keemail.me>
2021-12-23 07:13:11 -0600

    Repeat string Done // Hello World was done in Geneses

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
-};
+const repeatString = (str, int) => {
+    if(int < 0) {
+        return 'ERROR';
+    } else {
+        return str.repeat(int);
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
