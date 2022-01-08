1cdbcf736a94736bba6e17a4317dc294e35dccc4
Rodrigo <gokusan@keemail.me>
2022-01-08 08:17:22 -0600

    04 done remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
-
+"use strict";
+const removeFromArray = function(firstArray,...toRemove) {
+  let array = [...firstArray];
+  for (let i = 0; i < firstArray.length; i++) {
+      if (array.includes(toRemove[i])) {
+          array.splice(array.indexOf(toRemove[i]), 1)
+      }    
+  }
+  return array;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
