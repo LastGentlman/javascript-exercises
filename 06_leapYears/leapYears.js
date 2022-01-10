b39d458bc8bd13b5320884aab8d57451c6ef2cb0
Rodrigo <gokusan@keemail.me>
2022-01-10 13:23:01 -0600

    06 leapYear - Done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = (year) => {
+    let itIS = false;
+    if (year % 100 !== 0 && year % 4 === 0) {
+        itIS = true;
+    } else if (year % 400 === 0) {
+        itIS = true;
+    }
+    return itIS;
 };
 
 // Do not edit below this line
